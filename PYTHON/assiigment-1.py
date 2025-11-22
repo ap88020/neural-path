2f7e98533c4f572b8eaae7ad0efe5ff5507f3da3
Akash <Akash.patel.ap88020@gmail.com>
2025-11-22 18:02:46 +0530

    learnt besic of python

---
+++ PYTHON/assiigment-1.py
@@ -0,0 +1,34 @@
+# question-1
+
+# name = input("enter you name : ")
+# age = int(input("enter you age : "))
+
+# print("hi ", name , "you are " , age , "year old")
+
+# a = input("ente the value of a : ")
+# b = float(input("ente the value of b : "))
+
+# print(a+b)
+# print(a*b)
+# print(a/b)
+# print(a**b)
+
+# avg = (a+b)/2
+# print(avg)
+
+# print(type(int(a)),int(a))
+# print(type(float(a)),float(a))
+# print(type(str(a)),str(a))
+
+# temp = input("enter the tempature")
+# celsiusTemp = float(temp)
+
+# fahrenhietTemp = (celsiusTemp*(9/5))+32
+# print(fahrenhietTemp)
+
+radius = int(input("enter the num1 : "))
+pi = 3.14
+
+area = pi * radius**2
+
+print(area)
--- PYTHON/first.py
+++ PYTHON/first.py
@@ -1,1 +1,56 @@
-print("hllo world")
+# # print("hllo world")
+# age = 33
+# pi = 3.14
+# name = "akash"
+
+# # print(age,pi,name)
+# print(type(age))
+# print(type(pi))
+# print(type(name))
+
+# arthimitic
+a = 5
+b = 10
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+
+# relational & comparision operator
+
+# print(a < 5)
+# print(a > 5)
+# print(a == 5)
+# print(a != 5)
+
+# assignment
+
+# a += 10
+# print(a)
+# b -= 5
+# print(b)
+# a **= 5
+# print(a)
+
+# logical
+# print(not (a > b))
+
+# type conversion
+
+# print(type(int(b/a)))
+# val1 = int("123")
+# val2 = int("123")
+
+# print(val1+val2)
+
+# username = input("enter you name : ")
+# print(username)
+
+a = int(input("enter num1 : "))
+b = int(input("enter num2 : "))
+
+# print(a + b)
+avg = (a+b)/2
+print("avg of a and b : " , avg)
